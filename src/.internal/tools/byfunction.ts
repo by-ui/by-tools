f124197f9fbf84c21d94b4314ffd559bd4ec547e
肖康正 <452757438@qq.com>
2019-12-23 09:43:57 +0800

    update byfunction.ts

--- src/.internal/tools/byfunction.ts
+++ src/.internal/tools/byfunction.ts
@@ -1,18 +1,31 @@
 class byfunction {
 
-    static debounce(func: Function, wait: number) {
-        let timeout = null;
-        return function () {
-            if (timeout !== null)
-                clearTimeout(timeout);
-            timeout = setTimeout(func, wait);
+    static debounce(func: Function, wait: number, immediate: boolean = false) {
+        let timeout: any;
+        return function (this: any) {
+            let context = this;
+            let args = arguments;
+
+            if (timeout) clearTimeout(timeout);
+            if (immediate) {
+                let callNow = !timeout;
+                timeout = setTimeout(() => {
+                    timeout = null;
+                }, wait)
+                if (callNow) func.apply(context, args)
+            }
+            else {
+                timeout = setTimeout(() => {
+                    func.apply(context, args)
+                }, wait);
+            }
         }
     }
 
     static throttle(func: Function, delay: number) {
-        let timer = null;
+        let timer: any;
         let startTime = Date.now();
-        return function () {
+        return function (this: any) {
             const curTime = Date.now();
             const remaining = delay - (curTime - startTime);
             const context = this;
